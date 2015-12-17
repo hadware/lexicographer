b0df28c91d8df45b3f857f79dd56cee45db78503
Adrien Lacroix <oni.dev.box@gmail.com>
2015-12-17 15:28:26 +0100

    Add manager

--- src/epub_to_mongo.py
+++ src/epub_to_mongo.py
@@ -4,13 +4,31 @@ from mongo_manager import add_epub, remove_all_epub
 
 
 def store_epubs(epub_directory):
+    # get manager content
+    manager = os.path.join(epub_directory, '.manager')
+    # if file doesn't exist, create it
+    if not os.path.exists(manager):
+        open(manager, 'x').close()
+    # read lines
+    with open(manager, 'r') as f:
+        manager_content = f.readlines()
+
+    # for each epub in directory
     epub_list = os.listdir(epub_directory)
     for file in epub_list:
-        if file.endswith(".epub"):
-            nameFile = os.path.join(epub_directory, file)
-            print(nameFile)
-            jsonObject = epub_to_json(nameFile)
-            add_epub(jsonObject)
+        # if file not treated
+        if file + '\n' not in manager_content:
+            # if epub
+            if file.endswith(".epub"):
+                namefile = os.path.join(epub_directory, file)
+                # extract json
+                json = epub_to_json(namefile)
+                # add to database
+                add_epub(json)
+                # add to manager content
+                with open(manager, 'a') as f:
+                    f.write(file + '\n')
+                print(file + "done.")
 
 
 remove_all_epub()
