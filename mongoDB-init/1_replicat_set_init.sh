f31692bd8365775597650872a49b6158f4a5e1a6
Robin <robin12du84@hotmail.fr>
2015-12-20 08:22:58 -0800

    Add variables for IPs (not factorized yet)

--- mongoDB-init/1_replicat_set_init.sh
+++ mongoDB-init/1_replicat_set_init.sh
@@ -9,25 +9,26 @@ rm -rf data/config
 rm -rf data/shard*
 
 
-# start a replica set and tell it that it will be shard0(pc-robin 192.168.1.3)
-echo "starting servers for shard 0"
-mkdir -p data/shard0/rs0 data/shard0/rs1 data/shard0/rs2
-mongod --replSet s0 --logpath "s0-r0.log" --dbpath data/shard0/rs0 --port 37017 --fork --shardsvr --smallfiles
-mongod --replSet s0 --logpath "s0-r1.log" --dbpath data/shard0/rs1 --port 37018 --fork --shardsvr --smallfiles
-mongod --replSet s0 --logpath "s0-r2.log" --dbpath data/shard0/rs2 --port 37019 --fork --shardsvr --smallfiles
+# start a replica set and tell it that it will be shard0(pc-amadou 192.168.1.2)
+echo "starting servers for shard 1"
+mkdir -p data/shard1/rs0 data/shard1/rs1 data/shard1/rs2
+mongod --replSet s1 --logpath "s1-r0.log" --dbpath data/shard1/rs0 --port 47017 --fork --shardsvr --smallfiles
+mongod --replSet s1 --logpath "s1-r1.log" --dbpath data/shard1/rs1 --port 47018 --fork --shardsvr --smallfiles
+mongod --replSet s1 --logpath "s1-r2.log" --dbpath data/shard1/rs2 --port 47019 --fork --shardsvr --smallfiles
 
 sleep 5
 # connect to one server and initiate the set
-echo "Configuring s0 replica set"
+echo "Configuring s1 replica set"
 
-mongo --port 37017 << 'EOF'
+mongo --port 47017 << 'EOF'
 
 //Change this IP for different configuration !
-ip = "192.168.1.16"
+ip = "192.168.130.129"
 
-config = { _id: "s0", members:[
-          { _id : 0, host : ip + ":37017" },
-          { _id : 1, host : ip + ":37018" },
-          { _id : 2, host : ip + ":37019" }]};
+config = { _id: "s1", members:[
+          { _id : 0, host : ip + ":47017" },
+          { _id : 1, host : ip + ":47018" },
+          { _id : 2, host : ip + ":47019" }]};
 rs.initiate(config)
 EOF
+
--- mongoDB-init/4_enable_sharding.sh
+++ mongoDB-init/4_enable_sharding.sh
@@ -1,7 +1,12 @@
 # add shards and enable sharding on the test db
-mongo <<'EOF'
-sh.addShard("s0/192.168.1.3:37017")
-sh.addShard("s1/192.168.1.2:47017")
+mongo << 'EOF'
+
+//The IP address of all the other machine in the sharding
+my_ip="192.168.130.129"
+other_ip1="192.168.1.16"
+
+sh.addShard("s0/" + other_ip1 + ":37017")
+sh.addShard("s1/" + my_ip + ":47017")
 sh.enableSharding("epub")
 sh.shardCollection("epub.books",{"_id" : 1})
 EOF
