842ae3cd7077e855ba56e4999fbf7e73a7cf4ff1
jeanr84 <robin12du84@hotmail.fr>
2015-12-20 15:51:51 +0100

    Modif variable pour ip dans 1_replicat_set_init.sh

--- mongoDB-init/1_replicat_set_init.sh
+++ mongoDB-init/1_replicat_set_init.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # clean everything up
 echo "killing mongod and mongos"
 sudo killall mongod
@@ -6,24 +8,26 @@ echo "removing data files"
 rm -rf data/config
 rm -rf data/shard*
 
-echo "---------------sleeping for 5s-------------------"
+
+# start a replica set and tell it that it will be shard0(pc-robin 192.168.1.3)
+echo "starting servers for shard 0"
+mkdir -p data/shard0/rs0 data/shard0/rs1 data/shard0/rs2
+mongod --replSet s0 --logpath "s0-r0.log" --dbpath data/shard0/rs0 --port 37017 --fork --shardsvr --smallfiles
+mongod --replSet s0 --logpath "s0-r1.log" --dbpath data/shard0/rs1 --port 37018 --fork --shardsvr --smallfiles
+mongod --replSet s0 --logpath "s0-r2.log" --dbpath data/shard0/rs2 --port 37019 --fork --shardsvr --smallfiles
+
 sleep 5
+# connect to one server and initiate the set
+echo "Configuring s0 replica set"
 
-# start a replicate set and tell it that it will be a shard1 (pc-amadou 192.168.1.2)
-echo "starting servers for shard 1"
-mkdir -p data/shard1/rs0 data/shard1/rs1 data/shard1/rs2
-mongod --replSet s1 --logpath "s1-r0.log" --dbpath data/shard1/rs0 --port 47017 --fork --shardsvr --smallfiles
-mongod --replSet s1 --logpath "s1-r1.log" --dbpath data/shard1/rs1 --port 47018 --fork --shardsvr --smallfiles
-mongod --replSet s1 --logpath "s1-r2.log" --dbpath data/shard1/rs2 --port 47019 --fork --shardsvr --smallfiles
+mongo --port 37017 << 'EOF'
 
-echo "---------------sleeping for 10s--------------"
-sleep 10
+//Change this IP for different configuration !
+ip = "192.168.1.16"
 
-echo "Configuring s1 replica set"
-mongo --port 47017 << 'EOF'
-config = { _id: "s1", members:[
-          { _id : 0, host : "192.168.1.2:47017" },
-          { _id : 1, host : "192.168.1.2:47018" },
-          { _id : 2, host : "192.168.1.2:47019" }]};
+config = { _id: "s0", members:[
+          { _id : 0, host : ip + ":37017" },
+          { _id : 1, host : ip + ":37018" },
+          { _id : 2, host : ip + ":37019" }]};
 rs.initiate(config)
 EOF
