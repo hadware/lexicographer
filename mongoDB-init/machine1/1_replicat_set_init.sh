7a9625cd9a5407bb5173641ee347275958809c1e
jeanr84 <robin12du84@hotmail.fr>
2015-12-30 14:54:39 +0100

    MAJ machine1 script for mongo initialization

---
+++ mongoDB-init/machine1/1_replicat_set_init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# clean everything up
+echo "killing mongod and mongos"
+sudo killall mongod
+sudo killall mongos
+echo "removing data files"
+rm -rf data/config
+rm -rf data/shard*
+
+
+# start a replica set and tell it that it will be shard0(pc-robin 192.168.1.3)
+echo "starting servers for shard 0"
+mkdir -p data/shard0/rs0 data/shard1/rs0
+mongod --replSet s0 --logpath "s0-r0.log" --dbpath data/shard0/rs0 --port 37017 --fork --shardsvr --smallfiles
+mongod --replSet s1 --logpath "s1-r0.log" --dbpath data/shard1/rs0 --port 37018 --fork --shardsvr --smallfiles
+#mongod --replSet s0 --logpath "s0-r2.log" --dbpath data/shard0/rs2 --port 37019 --fork --shardsvr --smallfiles
+
+sleep 5
+# connect to one server and initiate the set
+echo "Configuring s0 replica set"
+
+mongo --port 37017 << 'EOF'
+
+//Change this IP for different configuration !
+ip = "192.168.1.16"
+
+config = { _id: "s0", members:[{ _id : 0, host : ip + ":37017" }]};
+rs.initiate(config)
+EOF
