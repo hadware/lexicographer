e32f0857c57a254513ccab09fd99eaa2285b3ba6
Robin <robin12du84@hotmail.fr>
2015-12-30 05:42:36 -0800

    test mongo replica set in 2 VM

--- mongoDB-init/4_enable_sharding.sh
+++ mongoDB-init/4_enable_sharding.sh
@@ -6,7 +6,7 @@ my_ip="192.168.130.129"
 other_ip1="192.168.1.16"
 
 sh.addShard("s0/" + other_ip1 + ":37017")
-sh.addShard("s1/" + my_ip + ":47017")
+sh.addShard("s1/" + my_ip + ":47018")
 sh.enableSharding("epub")
 sh.shardCollection("epub.books",{"_id" : 1})
 EOF
