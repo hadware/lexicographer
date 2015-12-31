4d37de1cbca2fd713739550021924f88c3604c8c
Ubuntu <jeanr@epub2.epub2.g4.internal.cloudapp.net>
2015-12-31 11:56:09 +0000

    Fix little errors

--- mongoDB-init/machine2/0_init_mongod.sh
+++ mongoDB-init/machine2/0_init_mongod.sh
@@ -13,6 +13,6 @@ echo "machine 2 is starting mongod instances on different shards"
 mkdir -p data/shard0/rs1 data/shard1/rs1 data/shard2/rs1
 mongod --replSet s0 --logpath "s0-r1.log" --dbpath data/shard0/rs1 --port 37017 --fork --shardsvr --smallfiles
 mongod --replSet s1 --logpath "s1-r1.log" --dbpath data/shard1/rs1 --port 37018 --fork --shardsvr --smallfiles
-mongod --replSet s2 --logpath "s2-r1.log" --dbpath data/shard1/rs2 --port 37019 --fork --shardsvr --smallfiles
+mongod --replSet s2 --logpath "s2-r1.log" --dbpath data/shard2/rs1 --port 37019 --fork --shardsvr --smallfiles
 
 sleep 10
