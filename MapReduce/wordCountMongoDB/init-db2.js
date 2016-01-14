0a17fc19e4f47ba1e4044c9120714b7bd3797374
Amadou Sall <ahasall@yahoo.fr>
2016-01-14 17:31:09 +0100

    add init-db2

---
+++ MapReduce/wordCountMongoDB/init-db2.js
@@ -0,0 +1,14 @@
+db.in.drop()
+db.in.insert({
+    "chapters": [
+        {
+            "name": "chapitre 1",
+            "text": "Je me demande combien de fois de ta mere et ton pere. merde ! etage ? Et toi ... Oui"
+        },
+        {
+            "name": "chapitre 2",
+            "text": "J'aime les femmes qui ont du caractère ; et les femmes qui baisent bien" 
+        }
+    ]
+});
+;
