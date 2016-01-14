2c8ead1f34a227f8cb5dc4db561d9332f0775709
Amadou Sall <ahasall@yahoo.fr>
2016-01-14 17:06:47 +0100

    updated init-db.js

--- MapReduce/wordCountMongoDB/init-db.js
+++ MapReduce/wordCountMongoDB/init-db.js
@@ -3,32 +3,12 @@ db.in.insert({
     "chapters": [
         {
             "name": "chapitre 1",
-            "text": "Nous n’allons pas seulement danser ou danser ou danser ou jouer au football, l'ensemble des 5 sens indispensable(s) en science et culture est censé(e) booster la bijectivité autour des dialogues intercommunautaires, merci"
+            "text": "Je me demande combien de fois de ta mere et ton pere merde etage \n Et toi ? Oui"
         },
         {
             "name": "chapitre 2",
-            "text": "Chapitre abstrait 3 du conpendium : la pédagogie de la Théorie Générale des Organisations suffit à catapulter mes frères propres dans la sous-régionalité, bonnes fêtes"
-        },
-        {
-            "name": "chapitre 2",
-            "text": "L’émergence ici c’est l’émulsion, c’est pas l’immersion donc la force vers ce qu’on appelle la dynamique des sports paraît effaceter cette climatologie possédant la francophonie, merci"
+            "text": "J'aime les femmes qui ont du caractère\net les femmes qui baisent bien" 
         }
     ]
 });
-
-db.in.insert({
-    "chapters": [
-        {
-            "name": "chapitre 1",
-            "text": "Pour emphysiquer l'animalculisme, la congolexicomatisation éventualiste sous cet angle là consiste à gérer la renaissance africaine autour des gens qui connaissent beaucoup de choses, merci"
-        },
-        {
-            "name": "chapitre 2",
-            "text": "Comme la coumbacérie ou le script de Aze, la prédestination à forciori, va aider la quatripartie dans ces prestances, c’est clair"
-        },
-        {
-            "name": "chapitre 2",
-            "text": "C’est à dire quand on parle de ces rollers, la congolexicomatisation vers ce qu’on appelle la dynamique des sports pousse à tourner le kilométrage vers l'humanisme, tu sais ça"
-        }
-    ]
-});
+;
